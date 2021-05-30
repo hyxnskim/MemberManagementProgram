16f9320df70c460c76115f85aa0736b74b34178f
hyxnskim <hyxnskim@users.noreply.github.com>
2021-05-30 23:43:29 +0900

    Added member attributes

--- memberManagement/src/model/dto/Member.java
+++ memberManagement/src/model/dto/Member.java
@@ -10,5 +10,33 @@ package model.dto;
  */
 
 public class Member {
-
+		// 필수 - 가입시 입력
+		/** 아이디, 식별키 */
+		private String memberId; // unique value
+		
+		/** 비밀번호, 필수 */
+		public String memberPw;
+		
+		/** 이름, 필수 */
+		public String name;
+		
+		/** 휴대폰, 필수 */
+		public String mobile;
+		
+		/** 이메일, 필수 */
+		public String email;
+		
+		// 필수 - 시스템
+		/** 가입일, 필수, 시스템 입력 */
+		public String entryDate;
+		
+		/** 등급, 필수, 시스템 입력 */
+		public String grade; // 일반(G) / 우수(S) / 관리자(A)
+		
+		// 선택
+		/** 마일리지, 선택, 시스템 입력 */
+		public int mileage;	// 일반회원 - 100,000이상이면 우수회원으로 등업
+		
+		/** 담당자, 선택, 시스템 입력 */
+		public String manager; // 우수회원에게 관리자 배정, can be NULL
 }
