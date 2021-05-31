62b251761f5ea7b3504ee162857404dd5f144b81
hyxnskim <hskim9706@gmail.com>
2021-06-01 00:17:57 +0900

    Initial commit

---
+++ MemberManagementProgram/src/model/dto/Member.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package model.dto;
+
+/**
+ * <pre>
+ * 회원의 속성(데이터) 항목
+ * </pre>
+ * @author Hyunsoo Kim
+ * @version ver.1.0
+ * @since jdk1.8
+ *
+ */
+public class Member {
+
+}
---
+++ MemberManagementProgram/src/view/Test.java
@@ -0,0 +1,20 @@
+package view;
+
+/**
+ * <pre>
+ * 회원 관리 프로그램 시작 클래스
+ *  * </pre>
+ * @author Hyunsoo Kim
+ * @version ver.1.0
+ * @since jdk1.8
+ */
+
+public class Test {
+
+	/**
+	 * 회원 관리 프로그램 시작 메서드
+	 * @param args 시작시 전달받은 String 타입 배열 정보
+	 */
+	public static void main(String[] args) {
+	}
+}
